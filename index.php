7a8b7c6b01964a285eb294a9e781bc529cecffad
Andrew Haller <stairwaytowonderland@gmail.com>
2018-10-18 06:35:35 -0400

    Created project index.php
    Demonstrates usage.

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Mobius Test</title>
+	<script type="text/javascript" src="js/core"></script>
+	<script type="text/javascript">
+		if (mobius && mobius.go) {
+			mobius.go();
+		}
+	</script>
+</head>
+<body>
+	<h1>Hello Mobius</h1>
+</body>
+</html>
